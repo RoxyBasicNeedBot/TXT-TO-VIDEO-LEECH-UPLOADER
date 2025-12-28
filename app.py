2ac175dc4881353e8262ebfd07dcda9d837acb9a
𝕽𝕺𝕏𝕐•𝔹𝕒𝕤𝕚𝕔ℕ𝕖𝕖𝕕𝔹𝕠𝕥 ⚡️ <235558257+RoxyBasicNeedBot@users.noreply.github.com>
2025-12-28 12:53:13 +0530

    Revert "Fixed some known bugs."

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-import os
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Well &  Good 😊 👍'
+    return 'https://t.me/roxybasicneedbot1'
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run()
